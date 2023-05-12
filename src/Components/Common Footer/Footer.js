906386f0bb73af47f4f86beba60003b32463e8ec
Ravi <ravimaragaani90@gmail.com>
2023-05-12 21:44:57 +0530

    fix lint errors

--- src/Components/Common Footer/Footer.js
+++ src/Components/Common Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { deleteCart, resetShoppingCart } from "../../store/shoppingSlice";
+import {resetShoppingCart } from "../../store/shoppingSlice";
 
 const Checkout = ({ type }) => {
   const navigate = useNavigate();
